40646150816a7e7c319130852ae714a3967caac6
Kaan İzgi <kaanizgi1531@gmail.com>
2022-08-18 18:42:34 +0300

    Initial Commit

---
+++ GithubAPI-Combine/GithubAPI_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubAPI_CombineApp.swift
+//  GithubAPI-Combine
+//
+//  Created by Kaan İzgi on 18.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct GithubAPI_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
